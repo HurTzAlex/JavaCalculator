7daafe2546500da4c89afc793f4594b091eb32a5
HurTzAlex <nattap0927@gmail.com>
2023-02-25 01:55:44 +0700

    Create Calculator GUI
    - number and mathematical symbol

---
+++ FrmCalculator.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+
+public class FrmCalculator {
+    private JPanel MainJPCalc;
+    private JTextField textField1;
+    private JRadioButton ONRadioButton;
+    private JRadioButton OFFRadioButton;
+    private JButton a7Button;
+    private JButton a8Button;
+    private JButton a9Button;
+    private JButton button4;
+    private JButton button5;
+    private JButton cButton;
+    private JButton button7;
+    private JPanel NumberJP;
+    private JPanel ClearJP;
+    private JButton a4Button;
+    private JButton a5Button;
+    private JButton a6Button;
+    private JButton button11;
+    private JButton a1Button;
+    private JButton a2Button;
+    private JButton a3Button;
+    private JButton button15;
+    private JButton a0Button;
+    private JButton button17;
+    private JButton button18;
+}
